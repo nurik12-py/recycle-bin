947f7e3253d4c09a9f32a4a884c9c0189aeb6bdf
nurik12-py <nurik.py.12@gmail.com>
2020-09-30 20:05:16 +0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Loading from "./Loading";
+var mqtt = require("mqtt");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var options = {
+  clientId: "01",
+  username: "sasha.petrov.olx@gmail.com",
+  password: "a6efcb63",
+  protocol: "MQTT",
+};
+
+var client = mqtt.connect("mqtt://mqtt.dioty.co:8080", options);
+
+const Status = styled.div`
+  background: ${(props) => getGreenToRed(props.height)};
+  width: 100px;
+  height: ${(props) => props.height > 18 && props.height + "%"};
+  z-index: 1;
+  border-radius: 10px;
+  font-size: 25px;
+  font-weight: 600;
+  &:after {
+    content: "${(props) => 357 - props.sm}см";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const getGreenToRed = (percent) => {
+  const r = (255 * percent) / 100;
+  const g = 255 - (255 * percent) / 100;
+  return "rgb(" + r + "," + g + ",0)";
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+  componentDidMount() {
+    client.subscribe("/sasha.petrov.olx@gmail.com/out", (data) => {
+      this.setState({ isLaoding: false });
+      console.log("connection+");
+    });
+    client.on("message", (topic, payload) => {
+      var height = (parseInt(payload.toString().split(":")[1]) / 27) * 100;
+      console.log(height);
+      if (!isNaN(height)) {
+        this.setState({
+          height: Math.floor(height),
+          sm: 357 - parseInt(payload.toString().split(":")[1]),
+        });
+      }
+    });
+    client.on("error", (error) => {
+      console.log("MQTT Error:", error);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h2>
+          Статус:{" "}
+          {this.state.height && this.state.height > 70
+            ? "Full"
+            : this.state.height > 40
+            ? "Medium"
+            : "Low"}
+        </h2>
+        <div className="trash">
+          {!this.state.isLaoding ? (
+            <Status height={this.state.height} sm={this.state.sm} />
+          ) : (
+            <Loading />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
